9c5ac5853c0f93017287cf8c1d377e3ced1967f5
kebejoan <jmailed.joe@gmail.com>
2025-08-24 17:13:12 +0700

    feat: implement route /main/timesheet/[pjtNo] and add component

--- src/app/main/projects/[pjtNo]/page.tsx
+++ src/app/main/projects/[pjtNo]/page.tsx
@@ -1,36 +1,178 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollBar } from "@/components/ui/scroll-area";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useAuth } from "@/hooks/useAuth";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as T from "@/types/tableTypes";
+import { fetchProjectByPjtNo } from "@/services/api/api.projects";
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
 
 interface PageProps {
   params: Promise<{ pjtNo: string }>;
 }
 
-function ProjectIdentity() {
+function ProjectPhase({ project }: { project?: T.Project }) {
+  let keys: (keyof T.Project)[] = [
+    "phase1EndDate",
+    "phase2EndDate",
+    "phase3EndDate",
+    "phase4EndDate",
+    "phase5EndDate",
+    "phase6EndDate",
+    "phase7EndDate",
+    "phase8EndDate",
+    "phase9EndDate",
+  ];
+
   return (
-    <Card className="w-full">
+    <Card className="w-full h-full">
       <CardHeader className="font-bold">
-        <CardTitle>Project Identity</CardTitle>
+        <CardTitle className="text-xl">Project Phase End Date</CardTitle>
+        <Separator />
       </CardHeader>
-      <CardContent>
-        <div>Pjt Name</div>
+      <CardContent className="w-full">
+        <div className="grid grid-cols-4">
+          {keys.map((key, index) => {
+            const dateStr = project?.[key] as string | undefined;
+            return (
+              <React.Fragment key={key}>
+                <div className="font-bold">Phase{index + 1}:</div>
+                <div key={index}>
+                  {dateStr ? new Date(dateStr).toLocaleDateString() : "N/A"}
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </div>
       </CardContent>
     </Card>
   );
 }
 
-export default async function Project({ params }: PageProps) {
-  const { session, auth } = useAuth();
+function ProjectAssignments({ project }: { project?: T.Project }) {
+  return (
+    <Card className="w-full h-full">
+      <CardHeader className="font-bold">
+        <CardTitle className="text-xl">Project Assignments</CardTitle>
+        <Separator />
+      </CardHeader>
+      <CardContent className="w-full">
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-3 gap-2">
+          <div className="font-bold col-span-1">Project Manager: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-2 2xl:col-span-2 text-wrap">
+            {project?.assignedPM?.name} {"("}
+            {project?.assignedPM?.code}
+            {")"}
+          </div>
+          <div className="font-bold col-span-1">System Engineer: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-2 2xl:col-span-2 text-wrap">
+            {project?.assignedSE?.name} {"("}
+            {project?.assignedSE?.code}
+            {")"}
+          </div>
+          <div className="font-bold col-span-1">Project Engineer: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-2 2xl:col-span-2 text-wrap">
+            {project?.assignedPE?.name} {"("}
+            {project?.assignedPE?.code}
+            {")"}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ProjectExecutionStatus({ project }: { project?: T.Project }) {
+  let currentPhaseEndDate: string | null = null;
+
+  if (project?.currentPhaseId) {
+    const key = `phase${project.currentPhaseId}EndDate` as keyof T.Project;
+    currentPhaseEndDate = project[key] as string;
+  }
+  return (
+    <Card className="w-full h-full">
+      <CardHeader className="font-bold">
+        <CardTitle className="text-xl">Execution Status</CardTitle>
+        <Separator />
+      </CardHeader>
+      <CardContent className="w-full">
+        <div className="flex flex-col items-center gap-1">
+          <Badge
+            className="font-bold text-2xl"
+            variant={
+              project?.executionStatus === T.ExecutionStatus.LAGGING
+                ? "destructive"
+                : "default"
+            }
+          >
+            {project?.executionStatus}
+          </Badge>
+          <Separator className="my-2" />
+          <span className=" text-">
+            {project?.currentPhase?.phaseCode} scheduled to be finished at:{" "}
+            {currentPhaseEndDate
+              ? new Date(currentPhaseEndDate).toLocaleDateString()
+              : "N/A"}
+          </span>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ProjectIdentity({ project }: { project?: T.Project }) {
+  return (
+    <Card className="w-full h-full">
+      <CardHeader className="font-bold">
+        <CardTitle className="text-xl">Project Identity</CardTitle>
+        <Separator />
+      </CardHeader>
+      <CardContent className="w-full">
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-7 gap-2">
+          <div className="font-bold col-span-1">Project Number: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-4 2xl:col-span-6 text-wrap">
+            {project?.pjtNo}
+          </div>
+          <div className="font-bold col-span-1">Project Name: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-4 2xl:col-span-6 text-wrap">
+            {project?.pjtName}
+          </div>
+          <div className="font-bold col-span-1">EPC: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-4 2xl:col-span-6 text-wrap">
+            {project?.epcName}
+          </div>
+          <div className="font-bold col-span-1">Owner: </div>
+          <div className="col-span-1 md:col-span-2 lg:col-span-4 2xl:col-span-6 text-wrap">
+            {project?.ownerName}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function Project({ params }: PageProps) {
+  const { auth } = useAuth();
   const [project, setProject] = useState<T.Project>();
+
+  useEffect(() => {
+    if (auth.isAuth) {
+      (async () => {
+        const pjtNo = (await params).pjtNo;
+        const data = await fetchProjectByPjtNo(pjtNo);
+        setProject(data);
+      })();
+    }
+  }, [auth.isAuth]);
 
   return (
     <>
       {/* <div>{(await params).pjtNo}</div> */}
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 w-full">
-        <span className="text-2xl font-bold">{(await params).pjtNo}</span>
+        <span className="text-2xl font-bold">{project?.pjtNo}</span>
         <ScrollArea className="bg-secondary p-0 rounded-lg flex justify-center items-center col-span-1 lg:col-span-2 2xl:col-span-4 h-[364px]">
           <div className="w-full h-[364px] p-4 flex justify-center items-center">
             <div className="bg-secondary outline-1 outline-primary p-1 rounded-lg w-[1550px] h-full flex justify-center items-center">
@@ -41,17 +183,17 @@ export default async function Project({ params }: PageProps) {
           </div>
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
-        <div className="bg-secondary p-0 rounded-lg col-span-1 lg:col-span-2 2xl:col-span-3">
-          <ProjectIdentity />
+        <div className="p-0 rounded-lg col-span-1 lg:col-span-2 2xl:col-span-3">
+          <ProjectIdentity project={project} />
         </div>
-        <div className="bg-secondary p-0 rounded-lg col-span-1">
-          <Card className="w-full"></Card>
+        <div className="p-0 rounded-lg col-span-1">
+          <ProjectExecutionStatus project={project} />
         </div>
-        <div className="bg-secondary p-0 rounded-lg col-span-1 2xl:col-span-2">
-          <Card className="w-full"></Card>
+        <div className="p-0 rounded-lg col-span-1 2xl:col-span-2">
+          <ProjectAssignments project={project} />
         </div>
-        <div className="bg-secondary p-0 rounded-lg col-span-1 2xl:col-span-2">
-          <Card className="w-full"></Card>
+        <div className="p-0 rounded-lg col-span-1 2xl:col-span-2">
+          <ProjectPhase project={project} />
         </div>
       </div>
     </>
