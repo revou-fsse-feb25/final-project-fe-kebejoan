46b266bed10e0e44448959bca261e1ec63744e6c
kebejoan <jmailed.joe@gmail.com>
2025-08-28 22:49:30 +0700

    feat: implement form for progress report create

--- src/app/main/progress/create/page.tsx
+++ src/app/main/progress/create/page.tsx
@@ -1,3 +1,107 @@
+"use client";
+
+import { ProgressInputFields } from "@/components/forms/progress/ProgressInputFields";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { useAuth } from "@/hooks/useAuth";
+import { getMyProjects, reportProgress } from "@/services/api/api.users-me";
+import { createProgressSchema } from "@/types/schemas";
+import { Project } from "@/types/tableTypes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { error } from "console";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+export type CreateProgressReportFormValues = z.infer<
+  typeof createProgressSchema
+>;
+
 export default function CreateProgressReport() {
-  return <div>Progress Report Create</div>;
+  const router = useRouter();
+  const { session, auth } = useAuth();
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const form = useForm<CreateProgressReportFormValues>({
+    resolver: zodResolver(createProgressSchema),
+    defaultValues: {
+      userId: session?.user.id,
+      projectId: undefined,
+      pjtPhaseId: undefined,
+      reportDate: undefined,
+      thisWeekTask: "",
+      thisWeekIssue: "",
+      nextWeekTask: "",
+      advancePhase: false,
+    },
+  });
+
+  useEffect(() => {
+    console.log("form error", form.formState.errors);
+    if (auth.isAuth || session) {
+      (async () => {
+        try {
+          const data = await getMyProjects();
+          setProjects(data);
+        } catch (error) {
+          console.error("Failed to fetch projects", error);
+        }
+      })();
+      form.reset({
+        userId: session?.user.id,
+        projectId: undefined,
+        pjtPhaseId: undefined,
+        reportDate: undefined,
+        thisWeekTask: "",
+        thisWeekIssue: "",
+        nextWeekTask: "",
+        advancePhase: false,
+      });
+    }
+  }, [auth.isAuth, session, form]);
+
+  const onSubmit = async (values: CreateProgressReportFormValues) => {
+    console.log("onsubmit values:", values);
+    try {
+      const res = await reportProgress(values);
+      if (!res) {
+        toast.error("Project Creation Failed");
+      } else {
+        toast.success("Project Created Successfully!");
+        router.push(`/main/progress/`);
+      }
+    } catch (err) {
+      toast.error("Failed to create project");
+    }
+  };
+
+  if (auth.isLoading) {
+    return (
+      <div className="container mx-auto py-6">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  return (
+    <div className="container mx-auto py-6">
+      <h1 className="text-2xl font-bold mb-6">Create New Project</h1>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit, (errors) =>
+            console.log("validation errors", errors)
+          )}
+          className="gap-4 w-full flex flex-col"
+        >
+          <ProgressInputFields form={form} projects={projects} />
+
+          <Button type="submit" className="w-full col-span-1 ">
+            Create Progress Report
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
---
+++ src/components/forms/progress/ProgressInputFields.tsx
@@ -0,0 +1,190 @@
+import { CreateProgressReportFormValues } from "@/app/main/progress/create/page";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { UseFormReturn } from "react-hook-form";
+import { Project } from "@/types/tableTypes";
+import { DatePickerField } from "../DatePicker";
+import { phaseAdvanceLookUp, phaseLookUp } from "@/types/lookUp";
+
+export function ProgressInputFields({
+  form,
+  projects,
+}: {
+  form: UseFormReturn<CreateProgressReportFormValues>;
+  projects: Project[];
+}) {
+  return (
+    <div className="col-span-1 xl:col-span-2 space-y-8">
+      <FormField
+        control={form.control}
+        name="userId"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>User ID</FormLabel>
+            <FormControl>
+              <Input type="text" disabled {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="projectId"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Select Project</FormLabel>
+            <FormControl>
+              <Select
+                onValueChange={(val) => field.onChange(Number(val))}
+                value={field.value ? String(field.value) : undefined}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select Project" />
+                </SelectTrigger>
+                <SelectContent>
+                  {projects?.map((p) => (
+                    <SelectItem key={p.id} value={p.id.toString()}>
+                      {p.pjtName}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="pjtPhaseId"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Select Project Phase</FormLabel>
+            <FormControl>
+              <Select
+                onValueChange={(val) => field.onChange(Number(val))}
+                value={field.value ? String(field.value) : undefined}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select Project Phase" />
+                </SelectTrigger>
+                <SelectContent>
+                  {phaseLookUp?.map((p) => (
+                    <SelectItem key={p.id} value={p.id.toString()}>
+                      {p.phaseName}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="reportDate"
+        render={({ field }) => (
+          <DatePickerField
+            label="Report Date"
+            date={field.value ? new Date(field.value) : undefined}
+            onChange={field.onChange}
+          />
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="thisWeekTask"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>This week's tasks</FormLabel>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Enter this week's tasks"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="thisWeekIssue"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>This week's tasks</FormLabel>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Enter this week's tasks"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="nextWeekTask"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Next week's tasks</FormLabel>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Enter next week's tasks"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="advancePhase"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Advance Phase?</FormLabel>
+            <FormControl>
+              <Select
+                onValueChange={(val) => field.onChange(val === "true")}
+                value={field.value ? String(field.value) : undefined}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Advance Phase" />
+                </SelectTrigger>
+                <SelectContent>
+                  {phaseAdvanceLookUp?.map((p) => (
+                    <SelectItem key={p.label} value={String(p.value)}>
+                      {p.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
